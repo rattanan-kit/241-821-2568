f2d63cd8e3858479d5be91d5892854cf24d8799c
rattanan-kit <rattanan.kit@ku.th>
2025-12-30 11:06:50 +0700

    java

---
+++ JS/main.js
@@ -0,0 +1,83 @@
+// /*
+// this is comment
+// */
+
+// console.log("hello world ")
+// console.log("This is java script")
+
+// const PI = 3.14
+
+// let fname = "john";
+
+// let age = 30;
+// let height = 5.8;
+
+// fname = "Alice"
+
+// console.log("name:",fname)
+// console.log("Age:",age)
+// console.log("Height:",height)
+
+
+
+// let number1 = "10";
+// let number2 = "3";
+
+// let result1 = number1 + number2;
+// console.log("ผลบวก: ",result1);
+
+// let condition = nuumber1 < number2;     // True
+// console.log("condition: ", condition);
+
+// let number1 = 3;
+// let number2 = 4;
+
+
+
+// let score = prompt("Enter your score: "); 
+// let grade = ''
+
+// if (score >= 80) {
+//     grade = "A";
+// } else if (score >= 70) {
+//     grade = "B";
+// } else if (score >= 60) {
+//     grade = "C";
+// } else if (score >= 50) {
+//     grade = "D";
+// } else {
+//     grade = "F";
+// }
+
+// console.log("Grade: ", grade);
+
+
+// let number1 = 0;
+// let number2 = 1;
+
+// let condition = (number1 > 0) && (number2 > 0); // False
+// console.log("condition: ", condition);
+
+// let condition2 = (number1 > 0) || (number2 > 0); // True
+// console.log("condition2: ", condition2);
+
+// let age = 25;
+// let gender = "female";
+
+// if (age >= 18 && gender == "female") {
+//     console.log("คุณเข้าร่วมได้");
+// }
+
+// let number1 = 21;
+
+// if (number1 % 2 != 0) {
+//     console.log("is Odd");
+// }
+// else {
+//     console.log("is Even");
+// }   
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log("For:", i);
+}   
