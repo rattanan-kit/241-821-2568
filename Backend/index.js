0758ffb06b3d14793144d8c59285ecfb7f6d779f
rattanan-kit <rattanan.kit@ku.th>
2026-03-10 11:15:28 +0700

    addd

--- Backend/index.js
+++ Backend/index.js
@@ -80,9 +80,39 @@ app.delete('/users/:id', async (req, res) => {
     }
 });
 
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstname) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastname) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกความสนใจอย่างน้อย 1 อย่าง');
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบายเกี่ยวกับตัวคุณ');
+    }
+    return errors;
+}
 app.post('/users', async (req, res) => {
     try {
     let user = req.body;
+    const errors = validateData(user);
+    if (errors.length > 0) {
+        throw {
+            message: 'กรอกข้อมูลไม่ครบถ้วน',
+            errors: errors
+        }
+    }
+
     const result = await conn.query('INSERT INTO users SET ?', user);
     console.log('result',result)
     res.json({
@@ -91,7 +121,9 @@ app.post('/users', async (req, res) => {
     });
 
     } catch (error) {
-        console.error('Error',error);
+        const errormessage = error.message || 'Error adding user';
+        const errors = error.errors || [];
+        console.error('Error',error.message);
         res.status(500).json({
             message: 'Error adding user',
             error: error
